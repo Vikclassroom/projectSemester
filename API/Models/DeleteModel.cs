a5cb41aac6c2853a01a3cabb0aaa86b3e6ab2181
Max <mmaj.classroom@gmail.com>
2021-03-12 14:54:56 +0100

    push

---
+++ API/Models/DeleteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class DeleteModel
+    {
+        [Key]
+        public int DeleteId { get; set; }
+        [Required]
+        public AccountModel AccountId { get; set; }
+        [Required]
+        public ListModel ListId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ API/Models/ListModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class ListModel
+    {
+        [Key]
+        public int ListId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Band { get; set; }
+        [Required]
+        public string Album { get; set; }
+    }
+}
