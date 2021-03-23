0615f8c3f5fb62bd3232dab30dd8dc0014919746
Max <mmaj.classroom@gmail.com>
2021-03-23 02:13:14 +0100

    API -> Endpoint AccountController Endpoint pour le controller Account presque terminé Gestion des erreurs basiques get get/id post put delete Manque endpoint LinkDto Manque endpoint Music Manque gestion de l'ajout d'une image de profil dans la partie account controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,5 @@
 ﻿using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -26,7 +25,7 @@ namespace API.Controllers
         public async Task<ActionResult<List<Account>>> GetAccounts()
         {
             var accounts = await _context.Accounts.ToListAsync();
-            
+
             return Ok(accounts);
         }
         //Get account/id
@@ -35,5 +34,78 @@ namespace API.Controllers
         {
             return await _context.Accounts.FindAsync(id);
         }
+
+        //Post
+        [HttpPost]
+        public async Task<ActionResult<Account>> CreateAccount([Bind("AccountId,Email,Password,UrlPicture")] Account account)
+        {
+            if (ModelState.IsValid) {
+                _context.AddAsync(account);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+
+            return StatusCode(500, "Une erreur est survenu lors de l'ajout du compte, une information est manquante");
+        }
+
+        //Put
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Account>> EditAccount(int id, [Bind("AccountId,Email,Password,UrlPicture")] Account account)
+        {
+            if (id != account.AccountId)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.AccountId))
+                    {
+                        return NotFound($"Le compte {id} n'a pas été trouvé");
+                    }
+                }
+            }
+            return NoContent();
+        }
+
+        //Delete
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Account>> DeleteAccount(int id)
+        {
+            try
+            {
+                var account = await _context.Accounts.FindAsync(id);
+
+                if (account == null)
+                {
+                    return NotFound($"Le compte {id} est introuvable");
+                }
+
+                _context.Accounts.Remove(account);
+                await _context.SaveChangesAsync();
+
+                return Ok($"Le compte avec l'{id} à bien été supprimé");
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Erreur à la suppression du compte");
+            }
+
+        }
+
+        //true or false
+        private bool AccountExists(int id)
+        {
+            return _context.Accounts.Any(e => e.AccountId == id);
+        }
     }
 }
+
